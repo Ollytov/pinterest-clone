b228f2b80776e45d64948a684938a3c7daf6a0f9
Ollytov <daniel.maloof@gmail.com>
2015-11-30 22:16:50 -0500

    mongodb url changed

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -5,7 +5,7 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://meanuser:meanpass@ds053774.mongolab.com:53774/heroku_b9bkvrxc'
+    uri: 'mongodb://meanuser:meanpass@ds059694.mongolab.com:59694/heroku_v7n6gsw0'
   },
 
   seedDB: true
