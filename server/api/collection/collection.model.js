fe55c521483d9e202bdd3715345b86aee1ff38d2
Ollytov <daniel.maloof@gmail.com>
2015-12-01 23:13:35 -0500

    changed collection model

--- server/api/collection/collection.model.js
+++ server/api/collection/collection.model.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var CollectionSchema = new Schema({
-  name: {type: String, lowercase: true},
+  name: String,
   author: String
 });
 
